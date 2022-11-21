ea333f4e1135d9e72cb5a2598581539b34c35c0c
pavan <awsdevops.pavan@gmail.com>
2022-11-21 06:38:07 +0530

    Variable enclosure for echo

--- echo.sh
+++ echo.sh
@@ -1,4 +1,12 @@
-for i in {1..10}
-do
-echo "Third time"
-done
+#!/bin/bash
+
+x=5
+echo "${x} is my lucky number" #varible enclosed in {}
+
+y=7
+z='$y' #variable enclosed in single quotes will be considered as string
+echo "the value of z is ${z}"
+
+b=15
+c="$b" #variable enclosed in double quotes will be considered as value
+echo "the value of c is ${c}"
