6fcf29f580821e8f179efe0c21f97725493f96d9
pavan <awsdevops.pavan@gmail.com>
2022-11-22 15:49:12 +0530

    Added two files base.h,get_Vpcs

---
+++ get_vpcs.sh
@@ -0,0 +1,22 @@
+{
+    "Vpcs": [
+        {
+            "CidrBlock": "172.31.0.0/16",
+            "DhcpOptionsId": "dopt-0dbb62d27ee4f5374",
+            "State": "available",
+            "VpcId": "vpc-06b3efc6b61ab29d1",
+            "OwnerId": "582469332580",
+            "InstanceTenancy": "default",
+            "CidrBlockAssociationSet": [
+                {
+                    "AssociationId": "vpc-cidr-assoc-0115eb4e9b51b99eb",
+                    "CidrBlock": "172.31.0.0/16",
+                    "CidrBlockState": {
+                        "State": "associated"
+                    }
+                }
+            ],
+            "IsDefault": true
+        }
+    ]
+}
