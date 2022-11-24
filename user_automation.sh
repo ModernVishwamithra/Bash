f5b02e047133703d40ff3594ebe51a0affd4c395
pavan <awsdevops.pavan@gmail.com>
2022-11-24 14:35:37 +0530

    created user automation file

---
+++ user_automation.sh
@@ -0,0 +1,10 @@
+# script that can take username(s) as input and validate the username(should not start with number) if already exists or NOT
+# if exists, show error message that username already exists other wise create a user with random password
+# Once login, make password expire and promt user to create a new password.
+
+echo "Enter the user name"
+user=$1 
+if [[${#user} -gt 0]]; then
+echo "User will be created"
+else
+echo "You have given ${#user}" 
