7afdbcab37b797e471e1d963f25600d8d468973d
pavan <awsdevops.pavan@gmail.com>
2022-11-26 21:28:38 +0530

    Testing get vpcs code with functions -2

--- AWS/get_vpcs.sh
+++ AWS/get_vpcs.sh
@@ -33,12 +33,14 @@ FORMAT="json"
 #uisng functions
 
 Get_VPC() {
+    for REGION in $@; do
     echo "Running The Functon To List VPCs in $1"
     vpc_list=$(aws ec2 describe-vpcs --region $1 | jq .Vpcs[].VpcId | tr -d '"')
     for vpc in $(echo $vpc_list); do
         echo "The VPC ID IS:$vpc"
         echo "======================================="
     done
+    done
 }
 
-Get_VPC $1
+Get_VPC $@
