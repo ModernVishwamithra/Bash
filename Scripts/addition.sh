daf9b07ad0674fedd2fb0c23d7d1d0730349abf0
pavan <awsdevops.pavan@gmail.com>
2022-11-26 21:06:14 +0530

    Added functions code -sum of two numbers-created file

--- Scripts/addition.sh
+++ Scripts/addition.sh
@@ -1,10 +1,10 @@
 
-# get_sum(){
-#     a=$1
-#     b=$2
-# SUM=$(expr $a + $b)
-#     echo "Sum of two numbers is = $SUM"
-# }
+get_sum(){
+    a=$1
+    b=$2
+SUM=$(expr $a + $b)
+    echo "Sum of two numbers is = $SUM"
+}
 
 function get_sum
 {
