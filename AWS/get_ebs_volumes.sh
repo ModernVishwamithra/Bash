89c97488eaef78790c13e2228737a35919b0188a
pavan <awsdevops.pavan@gmail.com>
2022-11-26 08:02:27 +0530

    Updated EBS volume empty check

--- AWS/get_ebs_volumes.sh
+++ AWS/get_ebs_volumes.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 REGIONS=$(aws ec2 describe-regions | jq ".Regions[].RegionName" | tr -d '"')
 for REGION in ${REGIONS[@]}; do 
-VOLUMES=$(aws ec2 describe-volumes --region $REGION)
+VOLUMES=$(aws ec2 describe-volumes --region $REGION | jq ".Volumes[]" )
 echo "volume count $()"
 for VOLUME in $VOLUMES; do
 echo "EBS volumes from region $REGION is $VOLUME"
