c35141a8e9e03e288ae2a429cfd89ea5e00a3d99
pavan <awsdevops.pavan@gmail.com>
2022-11-26 08:20:02 +0530

    Updated EBS volume empty check-test-5

--- AWS/get_ebs_volumes.sh
+++ AWS/get_ebs_volumes.sh
@@ -1,5 +1,6 @@
 #Script to get the ebs volumes based on the region and delete UNATTACHED volumes
 #!/bin/bash
+set -x
 REGIONS=$(aws ec2 describe-regions | jq ".Regions[].RegionName" | tr -d '"')
 for REGION in ${REGIONS[@]}; do 
 VOLUME_IDS=$(S# aws ec2 describe-volumes --region $REGION | jq ".Volumes[])
