377f64564a18bde1afbfcf8f8514c0cba46bb89c
pavan <awsdevops.pavan@gmail.com>
2022-11-26 20:46:24 +0530

    Added functions code -sum of two numbers

--- functions.sh
+++ functions.sh
@@ -1,3 +1,16 @@
+
+# Syntax1:
+# function function_name
+# {
+# ###set of commands
+# }
+
+# Syntax2:
+# function_name()
+# {
+# ####set of commands
+# }
+
 get_sum(){
     a=$1
     b=$2
@@ -5,4 +18,4 @@ SUM=$(expr $a + $b)
     echo "Sum of two numbers is = $SUM"
 }
 
-get_sum
+get_sum $1 $2
