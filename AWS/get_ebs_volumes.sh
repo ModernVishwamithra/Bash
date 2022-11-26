fcef47481e5e66f7eec62c7a360b38df028c53e1
pavan <awsdevops.pavan@gmail.com>
2022-11-26 17:52:42 +0530

    Final touchup -3 modified delete code

--- AWS/get_ebs_volumes.sh
+++ AWS/get_ebs_volumes.sh
@@ -7,14 +7,14 @@ if [[ $(aws ec2 describe-volumes --region $REGION | jq ".Volumes[].VolumeId") = 
 echo "No EBS volumes are present in this Region $REGION "
 else
 VOLUME_IDS=$(aws ec2 describe-volumes --region $REGION | jq ".Volumes[].VolumeId" | tr -d '"')
-echo "There are ${#VOLUME_IDS[@]} volumes are present in this region  $REGION "
+#echo "There are ${VOLUME_IDS[@]} volumes are present in this region  $REGION "
 for VOLUME_ID in ${VOLUME_IDS[@]}; do 
-echo "They are $VOLUME_ID"
+#echo "They are $VOLUME_ID"
 if [[ $(aws ec2 describe-volumes \
     --region $REGION \
-    --filters Name=status,Values=available Name=volume-id,Values=$VOLUME_ID| jq ".Volumes[].State" | tr -d '"') = "available" ]]; then
+    --filters Name=status,Values=available Name=volume-id,Values=$VOLUME_ID | jq ".Volumes[].State" | tr -d '"') = "available" ]]; then
     echo "Deleting unattached volume $VOLUME_ID"
-    echo "$(aws ec2 delete-volume --volume-id $VOLUME_ID)"
+    echo "$(aws ec2 delete-volume --volume-id $VOLUME_ID)" #2>/dev/null
     echo "$VOLUME_ID Deleted Successfully"
     else
     echo "Cannot able to delete $VOLUME_ID"
