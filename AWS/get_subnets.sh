bc184aa04971ca9729945aeea92fe8eb9d2adfcd
pavan <awsdevops.pavan@gmail.com>
2022-11-26 22:27:44 +0530

    Added subnets function with regions

--- AWS/get_subnets.sh
+++ AWS/get_subnets.sh
@@ -7,6 +7,7 @@ echo "Subnets for the region $REGION are"
 for SUBNET_ID in {echo "$(aws ec2 describe-subnets --region $REGION | jq ".Subnets[].SubnetId" | tr -d '"')"}; do
 echo "$SUBNET_ID"
 done
+done
 }
 
 get_subnets $@
