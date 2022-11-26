5d66e88e6d1800e5c4f8949f2c787951611e64cf
pavan <awsdevops.pavan@gmail.com>
2022-11-26 08:25:21 +0530

    Updated EBS volume empty check-test-9

--- AWS/get_ebs_volumes.sh
+++ AWS/get_ebs_volumes.sh
@@ -6,7 +6,7 @@ for REGION in ${REGIONS[@]}; do
 VOLUME_IDS=$(aws ec2 describe-volumes --region $REGION | jq ".Volumes[])
 VOLUME_ARRAY=($VOLUME_IDS)
 echo "volume Array is $VOLUME_ARRAY"
-
+done
 # for VOLUME in $(jq "VOLUMES); do
 # echo "EBS volumes from region $REGION is $VOLUME"
 # if [[ ${#VOLUMES} -gt 0 ]]; then
@@ -15,4 +15,3 @@ echo "volume Array is $VOLUME_ARRAY"
 # echo "EBS volumes from region $REGION is not available"
 # fi
 #done
-done
